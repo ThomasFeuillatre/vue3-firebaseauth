8f4538ac17d519d69a906ac8045ab730494346d5
Thomas Feuillatre <thomas.feuillatre.pro@gmail.com>
2022-10-01 18:29:17 +0200

    chore(nuxt): add docker to nuxt

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -2,9 +2,9 @@ import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    css: ['vuetify/lib/styles/main.sass', 'mdi/css/materialdesignicons.min.css'],
+    css: ['vuetify/lib/styles/main.sass', '@mdi/font/css/materialdesignicons.css'],
     modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss'],
-    server: { port: 8080 },
+    server: { host: '0.0.0.0' },
     build: {
         transpile: ['vuetify'],
         postcss: {
--- client/plugins/vuetify.js
+++ client/plugins/vuetify.js
@@ -1,13 +1,14 @@
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-import { defineNuxtPlugin } from "nuxt/app";
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import { defineNuxtPlugin } from 'nuxt/app';
+import '@mdi/font/css/materialdesignicons.css'; // Ensure you are using css-loader
 
-export default defineNuxtPlugin(nuxtApp => {
-  const vuetify = createVuetify({
-    components,
-    directives,
-  })
+export default defineNuxtPlugin((nuxtApp) => {
+    const vuetify = createVuetify({
+        components,
+        directives
+    });
 
-  nuxtApp.vueApp.use(vuetify)
-})
+    nuxtApp.vueApp.use(vuetify);
+});
